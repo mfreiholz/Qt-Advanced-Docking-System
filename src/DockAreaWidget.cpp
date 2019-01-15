d298a47bda54ee814eb7a61f8bf35a63da8a291a
Samuel Gaist <samuel.gaist@idiap.ch>
2019-01-15 17:24:11 +0100

    Removed unused constants

--- src/DockAreaWidget.cpp
+++ src/DockAreaWidget.cpp
@@ -1,17 +1,17 @@
 /*******************************************************************************
 ** Qt Advanced Docking System
 ** Copyright (C) 2017 Uwe Kindler
-** 
+**
 ** This library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU Lesser General Public
 ** License as published by the Free Software Foundation; either
 ** version 2.1 of the License, or (at your option) any later version.
-** 
+**
 ** This library is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ** Lesser General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU Lesser General Public
 ** License along with this library; If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
@@ -61,9 +61,6 @@ namespace ads
 {
 static const char* const INDEX_PROPERTY = "index";
 static const char* const ACTION_PROPERTY = "action";
-static const char* const DOCKWIDGET_PROPERTY = "dockwidget";
-static const int APPEND = -1;
-
 
 /**
  * New dock area layout mimics stack layout but only inserts the current
