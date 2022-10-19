6444e7424fcf571eb6168be53c481e5e44115af9
Chnossos <7984363+Chnossos@users.noreply.github.com>
2022-10-19 20:39:19 +0200

    fix: DockFocusController doesn't pick up on previous dock deletion outside of view (#453)
    Co-authored-by: Pierre Caissial <pierre.caissial@vivoka.com>

--- src/DockFocusController.cpp
+++ src/DockFocusController.cpp
@@ -42,7 +42,7 @@ struct DockFocusControllerPrivate
 	CDockFocusController *_this;
 	QPointer<CDockWidget> FocusedDockWidget = nullptr;
 	QPointer<CDockAreaWidget> FocusedArea = nullptr;
-	CDockWidget* OldFocusedDockWidget = nullptr;
+	QPointer<CDockWidget> OldFocusedDockWidget = nullptr;
 #ifdef Q_OS_LINUX
      QPointer<CFloatingDockContainer> FloatingWidget = nullptr;
 #endif
